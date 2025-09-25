655c5506a2dda4eaf839a1df73a2a07f19a4ce28
Arina M <thearinamarr@gmail.com>
2025-09-25 20:37:17 +0200

    refactor: fix typo in a comment

--- src/client/window.tracker.d.ts
+++ src/client/window.tracker.d.ts
@@ -4,12 +4,12 @@
  */
 import type { Tracker } from "./tracker.types.js";
 
-/*
+/**
  * In modules this is the way to augment global types.
  * See https://www.typescriptlang.org/docs/handbook/declaration-merging.html#global-augmentation
  * In a script we would omit `declare global` and use `interface Window { ... }` directly because
  * scripts always have global scope.
- * */
+ */
 declare global {
   interface Window {
     tracker?: Tracker;
